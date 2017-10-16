36e98c8228d83dedf2351f7fe8ad76bef7b7597f
justin.chan <justinixxe@gmail.com>
2017-10-16 18:26:03 +0800

    v1.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -217,3 +217,12 @@
 	});
 
 })(jQuery);
+
+
+//scroll
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
